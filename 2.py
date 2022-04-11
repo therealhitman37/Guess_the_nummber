b32a1ef32a25f4d1dd3f491a5b52d31009029cd9
therealhitman37 <102584348+therealhitman37@users.noreply.github.com>
2022-04-12 01:15:36 +0200

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,19 @@
+#guess the nummber
+
+import random
+
+x = int(input(" Enter a number: "))
+
+def guess(x):
+    random_number = random. randint(1,x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a nummber between 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high")
+
+    print(f" Yay. congrats. You have guesses the number{random_number} correctly")
+
+print(guess(x))
